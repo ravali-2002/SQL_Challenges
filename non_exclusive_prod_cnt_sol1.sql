801e5a112bb49b4b9ce9c3c02c9b878052331c20
Ravali K <115804596+ravali-2002@users.noreply.github.com>
2024-03-29 12:15:21 +0530

    Delete non_exclusive_prod_cnt_sol1.sql

--- non_exclusive_prod_cnt_sol1.sql
+++
@@ -1,7 +0,0 @@
--- sol : 1 using window functions
-with cte1 as(
-select product,vendor, row_number() over(partition by product order by product) r from tbl),
-cte2 as (
-select *, count(r) over(partition by product order by product) c from cte1)
-select vendor, sum(c) non_exclusive_products_cnt from cte2 where c=1
-group by vendor
